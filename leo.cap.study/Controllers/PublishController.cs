3baca9dedb23e3ca0ec03b7bad7f2d375e74c90b
leo_lcy <4106131001@qq.com>
2018-11-29 18:51:48 +0800

    CAP 消息队列

---
+++ leo.cap.study/Controllers/PublishController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCore.CAP;
+using leo.cap.study.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leo.cap.study.Controllers
+{
+    public class PublishController : Controller
+    {
+        private readonly ICapPublisher _capBus;
+
+        public PublishController(ICapPublisher capPublisher)
+        {
+            _capBus = capPublisher;
+        }
+
+        //不使用事务
+        [Route("~/without/transaction")]
+        public IActionResult WithoutTransaction(string msg="发送消息")
+        {
+            _capBus.Publish("xxx.services.show.time", msg);
+
+            return Ok();
+        }
+
+        //Ado.Net 中使用事务，自动提交
+        //[Route("~/adonet/transaction")]
+        //public IActionResult AdonetWithTransaction()
+        //{
+        //    using (var connection = new MySqlConnection(ConnectionString))
+        //    {
+        //        using (var transaction = connection.BeginTransaction(_capBus, autoCommit: true))
+        //        {
+        //            //业务代码
+
+        //            _capBus.Publish("xxx.services.show.time", DateTime.Now);
+        //        }
+        //    }
+        //    return Ok();
+        //}
+
+        //EntityFramework 中使用事务，自动提交
+        [Route("~/ef/transaction")]
+        public IActionResult EntityFrameworkWithTransaction([FromServices]AppDbContext dbContext)
+        {
+            using (var trans = dbContext.Database.BeginTransaction(_capBus, autoCommit: true))
+            {
+                //业务代码
+
+                _capBus.Publish("xxx.services.show.time", DateTime.Now);
+            }
+            return Ok("发送成功");
+        }
+
+
+
+
+        [CapSubscribe("xxx.services.show.time")]
+        public void CheckReceivedMessage(string msg)
+        {
+            Console.WriteLine(DateTime.Now+":"+msg);
+        }
+    }
+}
--- leo.cap.study/Startup.cs
+++ leo.cap.study/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using leo.cap.study.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -24,6 +26,40 @@ namespace leo.cap.study
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                   (b) =>
+                   {
+                       b.UseRowNumberForPaging();
+                       b.MigrationsAssembly("leo.cap.study");
+                   })
+           );
+
+            services.AddCap(x =>
+            {
+                //如果你使用的 EF 进行数据操作，你需要添加如下配置：
+                x.UseEntityFramework<AppDbContext>();  //可选项，你不需要再次配置 x.UseSqlServer 了
+
+                //如果你使用的Ado.Net，根据数据库选择进行配置：
+                //x.UseSqlServer("数据库连接字符串");
+                //x.UseMySql("Your ConnectionStrings");
+                //x.UsePostgreSql("Your ConnectionStrings");
+
+                //如果你使用的 MongoDB，你可以添加如下配置：
+                //x.UseMongoDB("Your ConnectionStrings");  //注意，仅支持MongoDB 4.0+集群
+
+                //如果你使用的 RabbitMQ 或者 Kafka 作为MQ，根据使用选择配置：
+                x.UseRabbitMQ(options=> {
+                    options.HostName = "123.207.70.217";
+                    options.Port = 5672;
+                    options.UserName = "guest";
+                    options.Password = "guest";
+                });
+                //x.UseKafka("localhost");
+
+                // 注册 Dashboard
+                x.UseDashboard();
+            });
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -51,6 +87,8 @@ namespace leo.cap.study
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            //app.UseCap();
 
             app.UseMvc(routes =>
             {
